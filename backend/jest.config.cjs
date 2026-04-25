e0681d6880514ed050164b1539689caf058ebe60
VitorHugo-Amaro <vitoorhugoamaro@hotmail.com>
2026-04-25 11:15:19 -0300

    instalando dependencia que faltava 2

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,11 +1,13 @@
 module.exports = {
-  preset: 'ts-jest/presets/default-esm', // Avisa que o TS deve ser convertido para ESM
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'], // Crítico: trata .ts como módulo
   transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }], // Traduz arquivos .ts usando ESM
-  },
-  extensionsToTreatAsEsm: ['.ts'], // Trata arquivos .ts como módulos modernos
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve problemas de extensão .js em imports do TS
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true, // Força o ts-jest a manter o formato de módulos
+      },
+    ],
   },
 };
