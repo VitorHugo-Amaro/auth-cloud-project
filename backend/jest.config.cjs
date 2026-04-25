fc723d2f62f8e3a0aab0e81e99c99e03725b041a
VitorHugo-Amaro <vitoorhugoamaro@hotmail.com>
2026-04-25 11:09:49 -0300

    solução para o conflito com typescript e o arquivo de config

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,4 +1,11 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
+  preset: 'ts-jest/presets/default-esm', // Avisa que o TS deve ser convertido para ESM
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }], // Traduz arquivos .ts usando ESM
+  },
+  extensionsToTreatAsEsm: ['.ts'], // Trata arquivos .ts como módulos modernos
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve problemas de extensão .js em imports do TS
+  },
 };
